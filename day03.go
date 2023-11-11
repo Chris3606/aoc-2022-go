31a4327b270b724b396761971f630235731eaa57
Chris3606 <cridley106@gmail.com>
2023-11-10 22:01:08 -0600

    Finished day 3 part 2.

--- day03.go
+++ day03.go
@@ -6,12 +6,13 @@ import (
 )
 
 type Rucksack struct {
+	allItems     map[byte]bool
 	compartment1 map[byte]bool
 	compartment2 map[byte]bool
 }
 
 func NewRucksack() Rucksack {
-	return Rucksack{map[byte]bool{}, map[byte]bool{}}
+	return Rucksack{map[byte]bool{}, map[byte]bool{}, map[byte]bool{}}
 }
 
 func parseInput3(input string) ([]Rucksack, error) {
@@ -31,9 +32,11 @@ func parseInput3(input string) ([]Rucksack, error) {
 		ruck := NewRucksack()
 		for i := range c1 {
 			ruck.compartment1[c1[i]] = true
+			ruck.allItems[c1[i]] = true
 		}
 		for i := range c1 {
 			ruck.compartment2[c2[i]] = true
+			ruck.allItems[c2[i]] = true
 		}
 
 		rucks = append(rucks, ruck)
@@ -50,6 +53,16 @@ func (ruck Rucksack) findSharedElem() byte {
 	}
 
 	panic("No shared element")
+}
+
+func findBadge(ruck1, ruck2, ruck3 Rucksack) byte {
+	for k := range ruck1.allItems {
+		if ruck2.allItems[k] && ruck3.allItems[k] {
+			return k
+		}
+	}
+
+	panic("No badge found")
 }
 
 func getPriority(item byte) int {
@@ -70,9 +83,16 @@ func Day03A(input string) int {
 	}
 
 	return sum
-
 }
 
 func Day03B(input string) int {
-	panic("nyi")
+	rucks, err := parseInput3(input)
+	CheckError(err)
+
+	var sum int
+	for i := 0; i < len(rucks); i += 3 {
+		sum += getPriority(findBadge(rucks[i], rucks[i+1], rucks[i+2]))
+	}
+
+	return sum
 }
--- main.go
+++ main.go
@@ -4,5 +4,5 @@ import "fmt"
 
 func main() {
 	fmt.Printf("Day 3A: %d\n", Day03A("inputs/day03.txt"))
-	//fmt.Printf("Day 3B: %d\n", Day02B("inputs/day02.txt"))
+	fmt.Printf("Day 3B: %d\n", Day03B("inputs/day03.txt"))
 }
